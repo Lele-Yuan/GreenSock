ed06b6bcee6b69b251c74843c3e8742450188b21
yuanlele <yuanlele@baidu.com>
2023-11-17 10:26:53 +0800

    feat: image name 删除.

---
+++ src/sq7/electric/index.js
@@ -0,0 +1,85 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+const initElectric = () => {
+    const isMobile = document.documentElement.offsetWidth < 431;
+    const systemName = isMobile ? 'mob' : 'pc';
+    
+    const initGsap = (sectionName, sectionConfig) => {
+        const {sectionCurrentFrame, canvasSize, sectionCount} = sectionConfig;
+        const sectionCanvas = document.getElementById(`${sectionName}_canvas`);
+        const sectionContext = sectionCanvas.getContext("2d");
+    
+        sectionCanvas.width = canvasSize.width;
+        sectionCanvas.height = canvasSize.height;
+    
+        const sectionImages = []
+        const sectionFrame = {frame: 0};
+    
+        for (let i = 0; i < sectionCount; i++) {
+            const img = new Image();
+            img.src = sectionCurrentFrame(i);
+            sectionImages.push(img);
+        }
+    
+        sectionImages[0].onload = render;
+    
+        function render() {
+            sectionContext.clearRect(0, 0, sectionCanvas.width, sectionCanvas.height);
+            sectionContext.drawImage(sectionImages[sectionFrame.frame], 0, 0, sectionCanvas.width, sectionCanvas.height);
+        }
+    
+        let sectionTl = gsap.timeline({
+        paused: true,
+        scrub: true,
+        scrollTrigger: {
+            trigger: `.section_scroll_wraper.${sectionName}`,
+            start: "top top",
+            end: 'bottom bottom',
+            scrub: true,
+            // markers: true,
+            pin: `.${sectionName} .section_cover_wrapper`,
+            anticipatePin: 1,
+        },
+        });
+        sectionTl.to(sectionFrame, {
+            frame: sectionCount - 1,
+            snap: "frame",
+            ease: "none",
+            duration: 1,
+            onUpdate: render, // use animation onUpdate instead of scrollTrigger's onUpdate
+        });
+    
+        if (!isMobile) {
+            sectionTl.to(
+                `.${sectionName} .section_mask`,
+                {opacity: 1, scrub: 1, duration: 1},
+                '>'
+            );
+        }
+    };
+    const indexName = index => {
+        if (index < 10) {
+            return `000${index}`
+        } else if (index < 100) {
+            return `00${index}`
+        } else {
+            return `0${index}`
+        }
+    }
+    const pathName = '/content/dam/OneWeb/faw_vw/model/q7/sq7/2023/animation/final';  // 线上图片路径
+    // const pathName = '/src/sq7'; // 本地图片路径
+    // 图片地址
+    const sectionCurrentFrame = index => `${pathName}/electric/jpg/${systemName}/cs1${indexName(index + 1)}.jpg`;
+    // const sectionCurrentFrame = index => `${pathName}/electric/webp/${systemName}/cs1${indexName(index + 1)}.webp`;
+    initGsap('section1', {
+        sectionCurrentFrame: sectionCurrentFrame,
+        canvasSize: {
+            pc: {width: 2880, height: 1620},
+            mob: {width: 1125, height: 2436}
+        }[systemName],
+        sectionCount: 101
+    });
+};
+initElectric();
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
     // headlight: './src/heatlight/headlight_gsap.js',
     // car: './src/car/car_gsap.js',
     // a6_avant: './src/a6_avant/car_gsap.js'
-    sq7: './src/sq7/index.js'
+    sq7_electric: './src/sq7/electric/index.js',
+    sq7_carface: './src/sq7/carface/index.js'
   }, //打包入口文件(如果是单个入口就是字符串，如果是多个入口就需要配置成对象)
   output: { //打包输出文件
       path: path.join(__dirname, 'dist'),
